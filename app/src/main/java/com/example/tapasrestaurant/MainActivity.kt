90372448738fefa235d5f342474e019ab4ef7736
Kinji Kuroda <jack.kaeorahan@student.gildeopleidingen.nl>
2022-10-10 13:50:29 +0200

    Ik ben Jack

--- app/src/main/java/com/example/tapasrestaurant/MainActivity.kt
+++ app/src/main/java/com/example/tapasrestaurant/MainActivity.kt
@@ -31,5 +31,6 @@ class MainActivity : AppCompatActivity() {
         // fab.setOnClickListener { view ->
             // Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                //  .setAction("Action", null).show()
+        // Hallo, ik ben Jack
         }
     }
